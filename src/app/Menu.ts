55a41cbf3f5bd8fcd91a01916db19f33e4af7456
Xue Hongyan <1141563180@qq.com>
2020-11-22 16:47:27 +0800

    Refactor

--- src/app/Menu.ts
+++ src/app/Menu.ts
@@ -2,253 +2,203 @@ import { BrowserWindow, KeyboardEvent, MenuItem, MenuItemConstructorOptions, she
 import { isDarwinPlatform } from '../utils/utils';
 import { store } from '../utils/utils';
 
-const menu = (window: any) => {
-    const tmpl: Array<MenuItemConstructorOptions> = [{
+const getMacOSFirstMenu: () => MenuItemConstructorOptions = () => ({
+    label: 'Gmail Desktop',
+    submenu: [{
+        label: 'About Overleaf Desktop',
+        role: 'about'
+    }, {
+        type: 'separator'
+    }, {
+        role: 'hide'
+    }, {
+        role: 'hideOthers'
+    }, {
+        role: 'unhide'
+    }, {
+        type: 'separator'
+    }, {
+        role: 'quit'
+    }]
+});
+
+const getProjectMenu: (window: any) => MenuItemConstructorOptions = (window) => ({
+    label: 'Project',
+    submenu: [{
         label: 'Projects',
-        submenu: [{
-            label: 'Projects',
-            click: async () => {
-                await window.loadURL('https://www.overleaf.com/project');
-            }
-        }]
+        click: async () => {
+            await window.loadURL('https://www.overleaf.com/project');
+        }
+    }]
+});
+
+const getEditMenu: () => MenuItemConstructorOptions = () => ({
+    label: 'Edit',
+    submenu: [{
+        role: 'undo'
     }, {
-        label: 'Edit',
-        submenu: [{
-            role: 'undo'
-        }, {
-            role: 'redo'
-        }, {
-            type: 'separator'
-        }, {
-            role: 'cut'
-        }, {
-            role: 'copy'
-        }, {
-            role: 'paste'
-        }, {
-            role: 'pasteAndMatchStyle'
-        }, {
-            role: 'delete'
-        }, {
-            role: 'selectAll'
-        }]
+        role: 'redo'
     }, {
-        label: 'View',
-        submenu: [{
-            role: 'toggleDevTools'
-        }, {
-            type: 'separator'
-        }, {
-            role: 'resetZoom'
-        }, {
-            role: 'zoomIn'
-        }, {
-            role: 'zoomOut'
-        }, {
-            type: 'separator'
-        }, {
-            type: 'checkbox',
-            label: 'Auto Hide Menubar',
-            checked: store.get('autoHideMenu'),
-            click: (menuItem: MenuItem, bw: BrowserWindow | undefined, evt: KeyboardEvent) => {
-                const newHide = !store.get('autoHideMenu');
-                if (newHide) {
-                    bw?.setAutoHideMenuBar(true);
-                } else {
-                    bw?.setAutoHideMenuBar(false);
-                    bw?.setMenuBarVisibility(true);
-                }
-                store.set('autoHideMenu', newHide);
-                menuItem.checked = newHide;
-            }
-        }, {
-            role: 'togglefullscreen'
-        }]
+        type: 'separator'
     }, {
-        label: 'Go',
-        submenu: [{
-            role: 'reload'
-        }, {
-            role: 'forceReload'
-        }]
+        role: 'cut'
     }, {
-        label: 'Help',
-        submenu: [{
-            label: 'Documentation',
-            click: async () => {
-                await shell.openExternal('https://www.overleaf.com/learn');
+        role: 'copy'
+    }, {
+        role: 'paste'
+    }, {
+        role: 'pasteAndMatchStyle'
+    }, {
+        role: 'delete'
+    }, {
+        role: 'selectAll'
+    }]
+});
+
+const getViewMenu: () => MenuItemConstructorOptions = () => ({
+    label: 'View',
+    submenu: [{
+        role: 'toggleDevTools'
+    }, {
+        type: 'separator'
+    }, {
+        role: 'resetZoom'
+    }, {
+        role: 'zoomIn'
+    }, {
+        role: 'zoomOut'
+    }, {
+        type: 'separator'
+    }, {
+        type: 'checkbox',
+        label: 'Auto Hide Menubar',
+        checked: store.get('autoHideMenu'),
+        click: (menuItem: MenuItem, bw: BrowserWindow | undefined, evt: KeyboardEvent) => {
+            const newHide = !store.get('autoHideMenu');
+            if (newHide) {
+                bw?.setAutoHideMenuBar(true);
+            } else {
+                bw?.setAutoHideMenuBar(false);
+                bw?.setMenuBarVisibility(true);
             }
-        }, {
-            label: 'Contact Us',
-            click: async () => {
-                await window.webContents.send('send-to-renderer', 'ContactUs', null);
-            }
-        }]
+            store.set('autoHideMenu', newHide);
+            menuItem.checked = newHide;
+        } }, {
+        role: 'togglefullscreen'
+    }]
+});
+
+const getMacOSViewMenu: () => MenuItemConstructorOptions = () => ({
+    label: 'View',
+    submenu: [{
+        role: 'toggleDevTools'
+    }, {
+        type: 'separator'
+    }, {
+        role: 'resetZoom'
+    }, {
+        role: 'zoomIn'
+    }, {
+        role: 'zoomOut'
+    }, {
+        type: 'separator'
+    }, {
+        role: 'togglefullscreen'
+    }]
+});
+
+const getGoMenu: () => MenuItemConstructorOptions = () => ({
+    label: 'Go',
+    submenu: [{
+        role: 'reload'
+    }, {
+        role: 'forceReload'
+    }]
+});
+
+const getHelpMenu: (window: any) => MenuItemConstructorOptions = (window) => ({
+    label: 'Help',
+    submenu: [{
+        label: 'Documentation',
+        click: async () => {
+            await shell.openExternal('https://www.overleaf.com/learn');
+        }
+    }, {
+        label: 'Contact Us',
+        click: async () => {
+            await window.webContents.send('send-to-renderer', 'ContactUs', null);
+        }
+    }]
+});
+
+const getAboutMenu: () => MenuItemConstructorOptions = () => ({
+    label: 'About',
+    submenu: [{
+        label: '© 2020 Overleaf'
+    }, {
+        type: 'separator'
+    }, {
+        label: 'Privacy and Terms',
+        click: async () => {
+            await shell.openExternal('https://www.overleaf.com/legal');
+        }
+    }, {
+        label: 'Security',
+        click: async () => {
+            await shell.openExternal('https://www.overleaf.com/legal#Security');
+        }
+    }, {
+        label: 'Contact Us',
+        click: async () => {
+            await shell.openExternal('https://www.overleaf.com/contact');
+        }
     }, {
         label: 'About',
-        submenu: [{
-            label: '© 2020 Overleaf'
-        }, {
-            type: 'separator'
-        }, {
-            label: 'Privacy and Terms',
-            click: async () => {
-                await shell.openExternal('https://www.overleaf.com/legal');
-            }
-        }, {
-            label: 'Security',
-            click: async () => {
-                await shell.openExternal('https://www.overleaf.com/legal#Security');
-            }
-        }, {
-            label: 'Contact Us',
-            click: async () => {
-                await shell.openExternal('https://www.overleaf.com/contact');
-            }
-        }, {
-            label: 'About',
-            click: async () => {
-                await shell.openExternal('https://www.overleaf.com/about');
-            }
-        }, {
-            label: 'Blog',
-            click: async () => {
-                await shell.openExternal('https://www.overleaf.com/blog');
-            }
-        }]
-    }];
+        click: async () => {
+            await shell.openExternal('https://www.overleaf.com/about');
+        }
+    }, {
+        label: 'Blog',
+        click: async () => {
+            await shell.openExternal('https://www.overleaf.com/blog');
+        }
+    }]
+});
 
+const getMacOSWindowMenu: () => MenuItemConstructorOptions = () => ({
+    label: 'Window',
+    submenu: [{
+        role: 'minimize'
+    }, {
+        role: 'close'
+    }, {
+        type: 'separator'
+    }]
+});
+
+const menu = (window: any) => {
+    const tmpl: Array<MenuItemConstructorOptions> = [
+        getProjectMenu(window),
+        getEditMenu(),
+        getViewMenu(),
+        getGoMenu(),
+        getHelpMenu(window),
+        getAboutMenu()
+    ];
     return tmpl;
 };
 
 const macOsMenu = (window: any) => {
-    const tmpl: Array<MenuItemConstructorOptions> = [{
-        label: 'Gmail Desktop',
-        submenu: [{
-            label: 'About Overleaf Desktop',
-            role: 'about'
-        }, {
-            type: 'separator'
-        }, {
-            role: 'hide'
-        }, {
-            role: 'hideOthers'
-        }, {
-            role: 'unhide'
-        }, {
-            type: 'separator'
-        }, {
-            role: 'quit'
-        }]
-    }, {
-        label: 'Projects',
-        submenu: [{
-            label: 'Projects',
-            click: async () => {
-                await window.loadURL('https://www.overleaf.com/project');
-            }
-        }]
-    }, {
-        label: 'Edit',
-        submenu: [{
-            role: 'undo'
-        }, {
-            role: 'redo'
-        }, {
-            type: 'separator'
-        }, {
-            role: 'cut'
-        }, {
-            role: 'copy'
-        }, {
-            role: 'paste'
-        }, {
-            role: 'pasteAndMatchStyle'
-        }, {
-            role: 'delete'
-        }, {
-            role: 'selectAll'
-        }]
-    }, {
-        label: 'View',
-        submenu: [{
-            role: 'toggleDevTools'
-        }, {
-            type: 'separator'
-        }, {
-            role: 'resetZoom'
-        }, {
-            role: 'zoomIn'
-        }, {
-            role: 'zoomOut'
-        }, {
-            type: 'separator'
-        }, {
-            role: 'togglefullscreen'
-        }]
-    }, {
-        label: 'Go',
-        submenu: [{
-            role: 'reload'
-        }, {
-            role: 'forceReload'
-        }]
-    }, {
-        label: 'Window',
-        submenu: [{
-            role: 'minimize'
-        }, {
-            role: 'close'
-        }, {
-            type: 'separator'
-        }]
-    }, {
-        label: 'Help',
-        submenu: [{
-            label: 'Documentation',
-            click: async () => {
-                await shell.openExternal('https://www.overleaf.com/learn');
-            }
-        }, {
-            label: 'Contact Us',
-            click: async () => {
-                await window.webContents.send('send-to-renderer', 'ContactUs', null);
-            }
-        }]
-    }, {
-        label: 'About',
-        submenu: [{
-            label: '© 2020 Overleaf'
-        }, {
-            type: 'separator'
-        }, {
-            label: 'Privacy and Terms',
-            click: async () => {
-                await shell.openExternal('https://www.overleaf.com/legal');
-            }
-        }, {
-            label: 'Security',
-            click: async () => {
-                await shell.openExternal('https://www.overleaf.com/legal#Security');
-            }
-        }, {
-            label: 'Contact Us',
-            click: async () => {
-                await shell.openExternal('https://www.overleaf.com/contact');
-            }
-        }, {
-            label: 'About',
-            click: async () => {
-                await shell.openExternal('https://www.overleaf.com/about');
-            }
-        }, {
-            label: 'Blog',
-            click: async () => {
-                await shell.openExternal('https://www.overleaf.com/blog');
-            }
-        }]
-    }];
-
+    const tmpl: Array<MenuItemConstructorOptions> = [
+        getMacOSFirstMenu(),
+        getProjectMenu(window),
+        getEditMenu(),
+        getMacOSViewMenu(),
+        getGoMenu(),
+        getMacOSWindowMenu(),
+        getHelpMenu(window),
+        getAboutMenu()
+    ];
     return tmpl;
 };
 
